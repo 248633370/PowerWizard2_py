177a2f89381c5e17a01c465825661a8da2e466d8
Anton <nar@necheta.ru>
2016-02-04 15:50:09 +0300

    pw2daemon - done, pw2client - работает

--- pw2client.py
+++ pw2client.py
@@ -15,51 +15,27 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import yaml
 import argparse
-
-from pymodbus.constants import Defaults
-# need for async TCP client
-#from twisted.internet import reactor, protocol
+import urllib
 
 #---------------------------------------------------------------------------# 
-# choose the requested modbus protocol
-#---------------------------------------------------------------------------#
-'''First step - only sync serial connection'''
-#from pymodbus.client.async import ModbusClientProtocol as ModbusClient
-from pymodbus.client.sync import ModbusSerialClient as ModbusSerialClient
-#from pymodbus.client.async import ModbusUdpClientProtocol as ModbusClient
+# state a few constants
+#---------------------------------------------------------------------------# 
+
+DEFAULT_SERVER = "zabbix.synmedia.ru"
+DEFAULT_PORT = 50280
+DEFAULT_URL = 'http://' + DEFAULT_SERVER + ':' + str(DEFAULT_PORT) + '/data.yaml'
+
+DEFAULT_PATH = os.path.dirname(__file__) + '/'
+DEFAULT_STORE = '/tmp/pw2py/'              # place to store 
+PID_FILE = DEFAULT_STORE + 'pw2c.pid'
+LOG_FILE = '/var/log/pw2c.log'
 
 #---------------------------------------------------------------------------# 
 # configure the client logging
 #---------------------------------------------------------------------------# 
 import logging
-logging.basicConfig()
+logging.basicConfig(format='%(asctime)s %(message)s', file=LOG_FILE)
 log = logging.getLogger()
-#log.setLevel(logging.DEBUG)
-
-#---------------------------------------------------------------------------# 
-# state a few constants
-#---------------------------------------------------------------------------# 
-DEFAULT_SERIAL_METHOD = "rtu"
-DEFAULT_SERIAL_STOPBITS = 1
-DEFAULT_SERIAL_BYTESIZE = 8
-DEFAULT_SERIAL_PARITY = "E"
-DEFAULT_SERIAL_BAUDRATE = 19200
-DEFAULT_SERIAL_TIMEOUT = 0.3
-DEFAULT_SERIAL_PORT = "/dev/ttyr00"        # default port for npreals
-#DEFAULT_SERIAL_PORT = "/dev/ttyS33"        # symlink to /dev/ttyr00
-
-#DEFAULT_TCP_SERVER = "172.16.156.69"
-#DEFAULT_TCP_PORT = 590
-#DEFAULT_TCP_ = 
-
-DEFAULT_REGS  = 201
-DEFAULT_UNIT = 0x01
-
-DEFAULT_PATH = os.path.dirname(__file__) + '/'
-DATA_FILE = DEFAULT_PATH + 'Params_wDataTypes.yaml'        # file with params config
-DEFAULT_STORE = '/tmp/pw2py/'              # place to store regs status
-PID_FILE = DEFAULT_STORE + 'pw2py.pid'
-LOG_FILE = '/var/log/pw2py.log'
 
 class Options(argparse.ArgumentParser):
     ''' argparse subclass  for operating options
@@ -69,18 +45,18 @@ class Options(argparse.ArgumentParser):
 
 class Params:
     '''class for manipulate Params ''' 
-    def __init__(self):
-        pass
+    def __init__(self, url=DEFAULT_URL):
+        self.yamldata = urllib.urlopen(url)
 
-    def load(self, yamlfile=DATA_FILE):
-        self.params = yaml.load(open(yamlfile, 'r'))
-        self.params['ParamID']['RegisterValue'] = '6'
-        self.params['ParamID']['Value'] = '9'
-        self.enabled_params = {}
-        for param in self.params.keys():
-            if self.params[param]['Enable'] == '1':
-                self.enabled_params[param] = self.params[param]
-        
+    def load(self):
+        #self.params = yaml.load(open(yamldata, 'r'))
+        try:
+            self.params = yaml.load(self.yamldata.read())
+        except IOError:
+            log.error('can not open url: Name or service not known')
+#        self.params['ParamID']['RegisterValue'] = '6'
+#        self.params['ParamID']['Value'] = '9'
+                
     def get_title(self):
         ''' return "title" of dictionary'''
         return self.params['ParamID'].keys()
@@ -111,13 +87,6 @@ class Params:
 
 def options_fill(options):
         ''' function for fill options '''
-#        options.add_argument('parameter',
-#                            type=str,
-#                            nargs='*',
-#                            help='query PW parameter')
-        options.add_argument('-a','--list-all',
-                            help='list all available parameters',
-                            action='store_true')
         options.add_argument('-g','--get-params',
                             type=str,
                             nargs='*',
@@ -126,47 +95,50 @@ def options_fill(options):
                             type=str,
                             nargs='+',
                             help='information about parameter')
-        options.add_argument('-l','--list-enable',
-                            help='list only enabled parameters',
+        options.add_argument('-l','--list-params',
+                            help='list ',
                             action='store_true')
         options.add_argument('-p','--port',
                             type=str,
-                            help='port for connect to panel')
+                            help='port for connect to daemon')
+        options.add_argument('-s','--server',
+                            type=str,
+                            help='server for connect to daemon')
+        options.add_argument('-u','--url',
+                            type=str,
+                            help='url for connect to daemon')
         options.add_argument('-t','--title',
                             help='list params title',
                             action='store_true')
 
-def print_params_table(param_id, column, tab=6):
+def print_params_table(param_dict, column, tab=6):
     ''' function for printing table.
-        param_id - list/dict of params that wil be printed; column - list of needed column from title;
-        Curent title is: ['ParamID', 'Enable', 'MinVal', 'MaxVal', 'Scale', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'DisplayText', 'Offset', 'NumUsedBits'] 
-        additionaly: 'RegisterValue', 'Value' '''
+        param_id - list/dict of params that will be printed; column - list of needed column from title;
+        Current title is: ['ParamID', 'Enable', 'MinVal', 'MaxVal', 'Scale', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'DisplayText', 'Offset', 'NumUsedBits'] 
+        additional: 'RegisterValue', 'Value' '''
     ''' Print table header '''
+    headerline = ''
     for key in column:
         try:
-            wtab = int(config.params['ParamID'][key])
-        except KeyError:
+            wtab = int(param_dict['ParamID'][key])
+        except (KeyError,ValueError):
             wtab = tab
         subkey = key[0:wtab]
         sys.stdout.write('|{0}'.format(subkey.ljust(wtab)))
+        headerline += '|' + '-' * wtab 
     print
-    for key in column:
-        try:
-            wtab = int(config.params['ParamID'][key])
-        except KeyError:
-            wtab = tab
-        sys.stdout.write('|{0}'.format(''.ljust(wtab,'-')))
-    print
+    print headerline
     ''' Print table '''
-    for param in sorted(param_id):
-        for key in column:
-            try:
-                wtab = int(config.params['ParamID'][key])
-            except KeyError:
-                wtab = tab
-            subkey = str(config.params[param][key]).decode('utf-8')[0:wtab]
-            sys.stdout.write('|{0}'.format(subkey.ljust(wtab)))
-        print
+    for param in sorted(param_dict.keys()):
+        if param <> 'ParamID':
+            for key in column:
+                try:
+                    wtab = int(param_dict['ParamID'][key])
+                except KeyError:
+                    wtab = tab
+                subkey = str(param_dict[param][key]).decode('utf-8')[0:wtab]
+                sys.stdout.write('|{0}'.format(subkey.ljust(wtab)))
+            print
         
 def check_pid():
     ''' Check PID for prevent two instances run 
@@ -199,29 +171,35 @@ if __name__ == "__main__":
     arguments = options.parse_args()
 
     ''' Load conf to dictionary '''
-    config = Params()
-    config.load()
+    try:
+        config = Params()
+        config.load()
+    except IOError:
+        log.error('stream IOError')
+        sys.exit()
 
     ''' List some info to stdout '''
     ''' {'ParamID', 'Enable', 'MinVal', 'MaxVal', 'Scale', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'DisplayText', 'Offset', 'NumUsedBits'} '''
-    if arguments.list_all:
-        print_params_table(config.params.keys(), ['ParamID', 'MinVal', 'MaxVal', 'Scale', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'DisplayText' ])
-        sys.exit()
-    elif arguments.list_enable:
-        print_params_table(config.enabled_params.keys(), ['ParamID', 'DisplayText', 'TotalBytes', 'WriteRegister', 'ReadRegister' ])
+    if arguments.list_params:
+        print_params_table(config.params, ['ParamID', 'DisplayText', 'TotalBytes', 'WriteRegister', 'ReadRegister' ])
         sys.exit()
     elif arguments.title:
         ''' get title '''
         print config.get_title()
         sys.exit()        
     elif arguments.param_info:
-        ''' Information for separate params '''
-        print_params_table(arguments.param_info, ['ParamID', 'DisplayText', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'Scale', 'Offset', 'MinVal', 'MaxVal' ])
+        info_params = {}
+        info_params['ParamID'] = config.params['ParamID']
+        for param in arguments.param_info:
+            info_params[param] = config.params[param]
+        print_params_table(info_params, ['ParamID', 'DisplayText', 'TotalBytes', 'WriteRegister', 'ReadRegister', 'Scale', 'Offset', 'MinVal', 'MaxVal' ])
         sys.exit()
     elif arguments.get_params is not None:
         ''' If used "-g" option, discard all other options '''
         if len(arguments.get_params) == 0:
-            for param in sorted(config.enabled_params.keys()):
+            get_params = {}
+            get_params['ParamID'] = config.params['ParamID']
+            for param in sorted(arguments.get_params):
                 arguments.get_params.append(param)
     elif len(sys.argv) == 1:
         ''' Wihtout scipt option(s) - get help '''
@@ -231,53 +209,4 @@ if __name__ == "__main__":
     ''' check pid befor main algorithm '''
     check_pid()
 
-    ''' Check override default arguments value '''
-    if arguments.port:
-        SERIAL_PORT = arguments.port
-    else:
-        SERIAL_PORT = DEFAULT_SERIAL_PORT
-
-    ''' main algorithm
-        init connection conf and connect'''
-    client = ModbusSerialClient( method=DEFAULT_SERIAL_METHOD, 
-                        port=SERIAL_PORT, 
-                        stopbits=DEFAULT_SERIAL_STOPBITS,
-                        bytesize=DEFAULT_SERIAL_BYTESIZE,
-                        parity=DEFAULT_SERIAL_PARITY,
-                        baudrate = DEFAULT_SERIAL_BAUDRATE,
-                        timeout = DEFAULT_SERIAL_TIMEOUT )
-    client.connect()
-    ''' get params and regs '''
-    for param in arguments.get_params:
-        try:
-            config.params[param]['RegisterValue']  = client.read_holding_registers(int(config.get_register(param))-1, 1, unit=DEFAULT_UNIT).registers[0]
-            scale = float(config.params[param]['Scale'].replace(',','.'))
-            offset = int(config.params[param]['Offset'])
-            min_val = float(config.params[param]['MinVal'].replace(',','.'))
-            max_val = float(config.params[param]['MaxVal'].replace(',','.'))
-            config.params[param]['Value'] = config.params[param]['RegisterValue'] * scale + offset
-            if (config.params[param]['Value'] > max_val) or (config.params[param]['Value'] < min_val):
-                ''' Out of range '''
-                config.params[param]['RegisterValue'] = 'ran_er'
-                config.params[param]['Value'] = 0
-        except AttributeError:
-            ''' Acquisition Error'''
-            config.params[param]['RegisterValue'] = 'acq_er'
-            config.params[param]['Value'] = 0
-        except (OSError):
-            ''' Port Error'''
-            config.params[param]['RegisterValue'] = 'prt_er'
-            config.params[param]['Value'] = 0
-
-    print_params_table(arguments.get_params, ['ParamID', 'RegisterValue', 'Value', 'DisplayText', 'ReadRegister' ])
-
-    ''' Close client connection '''
-    client.close()
-
-    '''test get params
-    print config.get_register(param_id='ParamID')
-    print config.get_description(param_id='ParamID')
-    print config.get_register(param_id='ENG_COOL_TMP')
-    print config.get_description(param_id='ENG_COOL_TMP')
-    '''
-
+    print_params_table(config.params, ['ParamID', 'RegisterValue', 'Value', 'DisplayText', 'ReadRegister' ])
